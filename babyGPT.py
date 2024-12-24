ab6af6bcd21070de827738e004f2722d593d48e9
Raghav <rpande2401@gmail.com>
2024-12-24 15:21:36 +1100

    Code changes.

--- babyGPT.py
+++ babyGPT.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+batch_size = 64 
+block_size = 256 
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
@@ -22,14 +22,13 @@ torch.manual_seed(1337)
 with open('./dataset/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-# here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
 
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
